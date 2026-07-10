4c919f46849daa2da0d87d59336760b1a35d9545
Pedro Correia <pedro.correia.2025097@my.istec.pt>
2026-07-10 20:39:32 +0100

    Update: criação do competitions.js . Este script define a lista de competições de futebol disponíveis na aplicação.

---
+++ ProjetoFinalPMDM/src/utils/competitions.js
@@ -0,0 +1,13 @@
+export const AVAILABLE_COMPETITIONS = [
+    { code: 'PPL', name: 'Primeira Liga' },
+    { code: 'PL', name: 'Premier League' },
+    { code: 'PD', name: 'La Liga' },
+    { code: 'SA', name: 'Serie A' },
+    { code: 'BL1', name: 'Bundesliga' },
+    { code: 'FL1', name: 'Ligue 1' },
+    { code: 'CL', name: 'Liga dos Campeões' },
+    { code: 'DED', name: 'Eredivisie' },
+    { code: 'BSA', name: 'Brasileirão' },
+    { code: 'ELC', name: 'Championship (Inglaterra)' },
+    { code: 'WC', name: 'Copa do Mundo' }
+]
