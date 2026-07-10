f29fb200e9c4cbf15085078dc176eaec4566d6f0
gui03322 <guilhermecardosoaraujo21@gmail.com>
2026-07-10 16:14:54 +0100

    organização do vite

--- ProjetoFinalPMDM/vite.config.js
+++ ProjetoFinalPMDM/vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // Evita erros de CORS ao chamar a football-data.org a partir do browser.
+      // O pedido sai do browser para /api/football/... e o Vite reencaminha-o
+      // para https://api.football-data.org/... do lado do servidor.
+      '/api/football': {
+        target: 'https://api.football-data.org',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/football/, ''),
+      },
+    },
+  },
 })
